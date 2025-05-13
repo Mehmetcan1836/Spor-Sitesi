0b5ca13fc9998f5c4ff13013348af56c7d8f3f24
Mehmetcan Çaycı <159884764+Mehmetcan1836@users.noreply.github.com>
2025-05-13 13:58:52 +0300

    deneme1

--- script.js
+++ script.js
@@ -4,3 +4,17 @@
   menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
   });
+    const boxes = document.querySelectorAll('.macplus-box');
+    const screenImage = document.getElementById('macplusScreenImage');
+    const originalScreen = screenImage.src;
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseenter', () => {
+            const newImage = box.getAttribute('data-image');
+            screenImage.src = newImage;
+        });
+        box.addEventListener('mouseleave', () => {
+            screenImage.src = originalScreen;
+        });
+    });
+
