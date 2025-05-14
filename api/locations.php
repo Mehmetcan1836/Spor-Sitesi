8698c470f6e231424b9af52ca3a7723b22bde55c
Mehmetcan Çaycı <159884764+Mehmetcan1836@users.noreply.github.com>
2025-05-14 17:30:02 +0300

    sasa

--- api/locations.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$locationsFile = '../locations.json';
-$locations = file_exists($locationsFile) ? json_decode(file_get_contents($locationsFile), true) : [];
-echo json_encode($locations);
